23afbaf1da7af871e50eadc30600d6db758c0326
Yewon Kim <sahrularsad@gmail.com>
2022-03-01 18:11:56 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,14 @@
 #!/bin/ash
+git clone https://github.com/SahrulGamerz/ptero-eggs ./temp
+cp -r ./temp/nginx ./
+cp -r ./temp/php-fpm ./
+cp -r ./temp/webroot ./
+cp ./temp/start.sh ./
+chmod +x ./start.sh
+rm -rf ./temp
 
 echo "Starting PHP-FPM..."
-/usr/sbin/php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
+php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
 echo "Starting Nginx..."
-/usr/sbin/nginx -c /home/container/nginx/nginx.conf
+nginx -c /home/container/nginx/nginx.conf
