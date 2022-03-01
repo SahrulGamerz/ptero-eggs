7ab531d1dfd8f7975f57f570a70e44cd2d149280
Yewon Kim <sahrularsad@gmail.com>
2022-03-01 20:08:35 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,17 @@
 #!/bin/ash
+echo Please start comment this section to prevent settings overwrite
+echo and change file permissions to 555
+
+git clone https://github.com/SahrulGamerz/ptero-eggs.git ./temp
+cp -r ./temp/nginx ./
+cp -r ./temp/php-fpm ./
+cp -r ./temp/webroot ./
+cp ./temp/start.sh ./
+chmod +x ./start.sh
+rm -rf ./temp
+
+echo End of comment
+
 echo "Starting PHP-FPM..."
 php-fpm8 --fpm-config /home/container/php-fpm/php-fpm.conf --daemonize
 
