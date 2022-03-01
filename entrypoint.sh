f8f30c236f4890bf4c693a652c2959f9aea32ee8
Yewon Kim <sahrularsad@gmail.com>
2022-03-01 19:43:28 +0800

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -8,4 +8,12 @@ MODIFIED_STARTUP=`eval echo $(echo ${STARTUP} | sed -e 's/{{/${/g' -e 's/}}/}/g'
 echo ":/home/container$ ${MODIFIED_STARTUP}"
 
 # Run the Server
+git clone https://github.com/SahrulGamerz/ptero-eggs /home/container/temp
+cp -r /home/container/temp/nginx /home/container/
+cp -r /home/container/temp/php-fpm /home/container/
+cp -r /home/container/temp/webroot /home/container/
+cp /home/container/temp/start.sh /home/container/
+chmod +x /home/container/start.sh
+rm -rf /home/container/temp
+
 ${MODIFIED_STARTUP}
